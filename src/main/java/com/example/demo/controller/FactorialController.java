985f521220f6dd85fbfe95696713f8c435cdbb4d
therealthom <tmorenopl@gmail.com>
2021-03-16 15:59:48 -0600

    first commit

---
+++ src/main/java/com/example/demo/controller/FactorialController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FactorialController {
+
+	@RequestMapping("/getFactorial")
+	public String helloworld() {
+		long factorial = 0;
+		for (int x = 0; x < 10; x++) {
+			factorial = factorial(20);
+		}		
+		System.out.println("Se calcula 10 veces el factorial de 20: " + factorial);
+		return "Se calcula 10 veces el factorial de 20: " + factorial;
+	}
+
+	public long factorial(int n) {
+        long resultado = 1;
+        for (int i = 1; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
+
